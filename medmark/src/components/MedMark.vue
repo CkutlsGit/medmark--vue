83f3b4bf1a914359a60f88a6d88c99b8deb40347
Kostya <ckutls228@gmail.com>
2023-10-28 17:06:02 +0300

    style: add section intro in medmark

--- medmark/src/components/MedMark.vue
+++ medmark/src/components/MedMark.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="header">
+    <section>
+        <div class="header">
         <div class="header__content">
             <h2 class="header__logo">MedMark</h2>
         </div>
     </div>
+    </section>
+    <section class="intro">
+    </section>
 </template>
 
 <script>
@@ -12,4 +16,6 @@ export default {
 }
 </script>
 
-<style src="../../public/main.css"></style>
+<style src="../../public/main.css">
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Poppins:wght@400;600&display=swap');
+</style>

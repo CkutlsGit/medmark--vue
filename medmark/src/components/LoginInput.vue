eb64f247cd6ff1921808551434fbc222dbbd137a
Kostya <ckutls228@gmail.com>
2023-10-28 17:34:10 +0300

    feat: add logical for logininput

--- medmark/src/components/LoginInput.vue
+++ medmark/src/components/LoginInput.vue
@@ -1,9 +1,32 @@
 <template>
-    
+    <section class="login">
+        <h1>Can you write login?</h1>
+        {{ login }}
+        <input 
+            type="text"
+            v-model.trim="login"
+        >
+        <button
+            type="button"
+            @click="confirm = true"
+        >
+            Подтвердить
+        </button>
+    </section>
 </template>
 
 <script>
+export default {
+    name: 'LoginInput',
+    data() {
+        return {
+            confirm: false,
+            login: ''
+        }
+    }
+}
 </script>
 
 <style>
+
 </style>

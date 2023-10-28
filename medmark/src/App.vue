531c7be8e1d26c30cedd7711ccef29a2fb2c59cd
Kostya <ckutls228@gmail.com>
2023-10-28 16:46:26 +0300

    feat: add default template for medmark

--- medmark/src/App.vue
+++ medmark/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="app">
-
+    <MedMark></MedMark>
   </div>
 </template>
 
 <script>
-
+import MedMark from './components/MedMark.vue';
 
 export default {
   name: 'App',
   components: {
-
+    MedMark
   }
 }
 </script>
---
+++ medmark/src/components/MedMark.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="root">
+        <h1>Hello</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MedMark',
+}
+</script>
+
+<style>
+
+</style>
